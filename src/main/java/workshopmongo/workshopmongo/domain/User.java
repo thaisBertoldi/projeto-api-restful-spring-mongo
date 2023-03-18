bd91af17b05e69a46042c500a0402ef2bbe043e4
Thais B <thais_brt@hotmail.com>
2023-03-18 16:54:43 -0300

    feat: Conexão com mongoDB com repository e service

--- src/main/java/workshopmongo/workshopmongo/domain/User.java
+++ src/main/java/workshopmongo/workshopmongo/domain/User.java
@@ -2,10 +2,15 @@ package workshopmongo.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user") // se for só Document ele vai interpretar que o nome da coleção é o nome da classe com letra minúscula
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/workshopmongo/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package workshopmongo.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import workshopmongo.workshopmongo.domain.User;
+import workshopmongo.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+    
+}
