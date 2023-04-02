4f23bc2dcaf3459ad351bf197c0be60be1d1f299
Thais B <thais_brt@hotmail.com>
2023-04-02 19:05:56 -0300

    feat: Consulta com vários critérios com @Query

--- src/main/java/workshopmongo/workshopmongo/repository/PostRepository.java
+++ src/main/java/workshopmongo/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package workshopmongo.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,5 +16,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> searchTitle(String title);
     
     List<Post> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
